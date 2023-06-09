d23e0e03d318c5f6e47bb8302ed86ca5aa3b5f96
Mark Youssef <mark.yssf@gmail.com>
2023-06-08 23:20:58 -0700

    Update tech grid for tablet

--- src/app/components/TechGrid/index.tsx
+++ src/app/components/TechGrid/index.tsx
@@ -12,7 +12,7 @@ const TechGrid: React.FC<TechGridProps> = ({ title, items }) => {
   return (
     <>
       <h3 className="text-2xl mb-4">{title}</h3>
-      <ul className="grid grid-cols-3 items-end lg:grid-cols-6 gap-4 mb-16">
+      <ul className="grid grid-cols-3 items-end md:grid-cols-4 lg:grid-cols-6 gap-4 mb-16">
         {items.map((item) => (
           <li key={item.name}>
             <TechIcon name={item.name} src={item.src} srcType={item.srcType} />
